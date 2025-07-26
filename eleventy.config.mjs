f4f924f2f4f1e6a8b4f80f20097154cbd723cc9a
malte <malte.fillmer@gmail.com>
2025-07-26 17:10:56 +0200

    add pagefind

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,13 @@
+import { execSync } from "child_process";
+
 /** @param {import("@11ty/eleventy").ThemeConfig} eleventyConfig */
 export default async function (eleventyConfig) {
+  eleventyConfig.on("eleventy.after", () => {
+    execSync(`npx pagefind --source _site --glob \"**/*.html\"`, {
+      encoding: "utf-8",
+    });
+  });
+
   return {
     ...eleventyConfig,
     dir: {
