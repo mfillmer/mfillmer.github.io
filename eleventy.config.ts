c5792bd4e66cee29d6b5652854244d506335b0fe
malte <malte.fillmer@gmail.com>
2025-07-30 19:17:01 +0200

    move pagefind build to npm scripts

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -2,12 +2,6 @@ import defineConfig from "11ty.ts";
 import { execSync } from "child_process";
 
 export default defineConfig((eleventyConfig) => {
-  eleventyConfig.on("eleventy.after", () => {
-    execSync(`npx pagefind --site _site --glob \"**/*.html\"`, {
-      encoding: "utf-8",
-    });
-  });
-
   eleventyConfig.addPassthroughCopy("webcomponents");
 
   return {
